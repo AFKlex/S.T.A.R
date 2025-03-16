81f59e9c958d34dad851d7bcea18e721b5d3c3b3
afklex <agricolaalex@gmail.com>
2025-03-16 12:18:02 +0100

    Integrate new chanes into main loop

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 from src.Node import * 
 from src.GraphManager import *
+from src.NmapScan import * 
 
 if __name__ == "__main__":
-    manager = GraphManager()
+    graph_manager = GraphManager()
+    node_manager = NodeManager()
 
-    host_a = Node("Host_A",serverity=9.5)
+    #host_a = Node("Host_A",serverity=9.5)
 
-    host_a.set_attribute({"IP":"10.10.10.10","OS":"Windows 10"})
+    #host_a.set_attribute({"IP":"10.10.10.10","OS":"Windows 10"})
 
-    host_b = Node("Host_B",serverity=5.9)
-    host_c = Node("Host_C")
+    #host_b = Node("Host_B",serverity=5.9)
+    #host_c = Node("Host_C")
 
-    manager.add_node(host_a)
-    manager.add_node(host_b)
-    manager.add_node(host_c)
+    #graph_manager.add_node(host_a)
+    #graph_manager.add_node(host_b)
+    #graph_manager.add_node(host_c)
 
-    edge = Edge(host_a,host_b, label="Fuck you")
-    edge2 = Edge(host_a,host_c, label="Fuck you")
-    edge3 = Edge(host_a,host_a, label="Fuck you 2")
+    #edge = Edge(host_a,host_b, label="Fuck you")
+    #edge2 = Edge(host_a,host_c, label="Fuck you")
+    #edge3 = Edge(host_a,host_a, label="Fuck you 2")
 
-    manager.add_edge(edge)
-    manager.add_edge(edge2)
-    manager.add_edge(edge3)
+    #graph_manager.add_edge(edge)
+    #graph_manager.add_edge(edge2)
+    #graph_manager.add_edge(edge3)
 
-    manager.generate_graph()
+    NmapScan("./data/nmap.xml",node_manager)
+
+    for entry in node_manager.list_nodes():
+        print(entry)
+
+    graph_manager.load_node_manager(node_manager)
+    
+    graph_manager.generate_graph()
 
 
 
